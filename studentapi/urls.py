11ad7274fed2c1b7ed1f95606d8a00aa1e3e5f5a
sidharths <sidharthsuresh599@gmail.com>
2024-04-03 12:41:53 +0530

    quiz

--- studentapi/urls.py
+++ studentapi/urls.py
@@ -9,7 +9,7 @@ router.register("jobs",views.jobView,basename="job_list"),
 router.register("applicationstatus",views.ApplicationStatusView,basename="applicationstatus"),
 router.register("interviews",views.InterviewView,basename="interview_list"),
 router.register("materials",views.MaterialView,basename="materials_list"),
-
+router.register("quiz",views.QuizView,basename="quiz"),
 
 
 urlpatterns=[
